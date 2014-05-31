dd78590a2c1d7a951651abcfcaa5dfc594b25092
C.H. Huang <plushuang.tw@gmail.com>
2014-05-31 12:24:47 +0800

    change version to 1.97

--- ui-gtk/UgtkConfig.h
+++ ui-gtk/UgtkConfig.h
@@ -52,7 +52,7 @@ extern "C" {
 #endif
 
 #ifndef PACKAGE_VERSION
-#define PACKAGE_VERSION		"1.95"
+#define PACKAGE_VERSION		"1.97"
 #endif
 
 // ----------------------------------------------------------------------------
