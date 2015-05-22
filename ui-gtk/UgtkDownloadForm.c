048a740ac2a009e3ab6a2710ebee0bd168ad5e87
C.H. Huang <plushuang.tw@gmail.com>
2015-05-22 22:41:46 +0800

    remove CR(\r) LF(\n) character from DownloadForm's Entry.

--- ui-gtk/UgtkDownloadForm.c
+++ ui-gtk/UgtkDownloadForm.c
@@ -459,14 +459,17 @@ void  ugtk_download_form_get (UgtkDownloadForm* dform, UgetNode* node)
 	text = gtk_entry_get_text ((GtkEntry*)dform->folder_entry);
 	ug_free (common->folder);
 	common->folder = (*text) ? ug_strdup (text) : NULL;
+	ug_str_remove_crlf (common->folder);
 	// user
 	text = gtk_entry_get_text ((GtkEntry*)dform->username_entry);
 	ug_free (common->user);
 	common->user = (*text) ? ug_strdup (text) : NULL;
+	ug_str_remove_crlf (common->user);
 	// password
 	text = gtk_entry_get_text ((GtkEntry*)dform->password_entry);
 	ug_free (common->password);
 	common->password = (*text) ? ug_strdup (text) : NULL;
+	ug_str_remove_crlf (common->password);
 	// retry_limit
 	number = gtk_spin_button_get_value_as_int ((GtkSpinButton*) dform->spin_retry);
 	common->retry_limit = number;
@@ -492,6 +495,7 @@ void  ugtk_download_form_get (UgtkDownloadForm* dform, UgetNode* node)
 		text = gtk_entry_get_text ((GtkEntry*)dform->uri_entry);
 		ug_free (common->uri);
 		common->uri = (*text) ? ug_strdup (text) : NULL;
+		ug_str_remove_crlf (common->uri);
 		if (common->uri) {
 			ug_uri_init (&uuri, common->uri);
 			// set user
@@ -518,12 +522,14 @@ void  ugtk_download_form_get (UgtkDownloadForm* dform, UgetNode* node)
 		text = gtk_entry_get_text ((GtkEntry*)dform->mirrors_entry);
 		ug_free (common->mirrors);
 		common->mirrors = (*text) ? ug_strdup (text) : NULL;
+		ug_str_remove_crlf (common->mirrors);
 	}
 	// file
 	if (gtk_widget_is_sensitive (dform->file_entry) == TRUE) {
 		text = gtk_entry_get_text ((GtkEntry*)dform->file_entry);
 		ug_free (common->file);
 		common->file = (*text) ? ug_strdup (text) : NULL;
+		ug_str_remove_crlf (common->file);
 	}
 
 	// ------------------------------------------
@@ -533,18 +539,22 @@ void  ugtk_download_form_get (UgtkDownloadForm* dform, UgetNode* node)
 	text = gtk_entry_get_text ((GtkEntry*) dform->referrer_entry);
 	ug_free (http->referrer);
 	http->referrer = (*text) ? ug_strdup (text) : NULL;
+	ug_str_remove_crlf (http->referrer);
 	// cookie_file
 	text = gtk_entry_get_text ((GtkEntry*) dform->cookie_entry);
 	ug_free (http->cookie_file);
 	http->cookie_file = (*text) ? ug_strdup (text) : NULL;
+	ug_str_remove_crlf (http->cookie_file);
 	// post_file
 	text = gtk_entry_get_text ((GtkEntry*) dform->post_entry);
 	ug_free (http->post_file);
 	http->post_file = (*text) ? ug_strdup (text) : NULL;
+	ug_str_remove_crlf (http->post_file);
 	// user_agent
 	text = gtk_entry_get_text ((GtkEntry*) dform->agent_entry);
 	ug_free (http->user_agent);
 	http->user_agent = (*text) ? ug_strdup (text) : NULL;
+	ug_str_remove_crlf (http->user_agent);
 
 	// ------------------------------------------
 	// UgetNode
