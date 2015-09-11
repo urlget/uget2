613726d82357cd130703ec299a76aa80b5bf9cd3
C.H. Huang <plushuang.tw@gmail.com>
2015-09-11 19:39:23 +0800

    change version to 2.0.2

--- ui-gtk/UgtkConfig.h
+++ ui-gtk/UgtkConfig.h
@@ -52,7 +52,7 @@ extern "C" {
 #endif
 
 #ifndef PACKAGE_VERSION
-#define PACKAGE_VERSION		"2.0.1"
+#define PACKAGE_VERSION		"2.0.2"
 #endif
 
 // ----------------------------------------------------------------------------
