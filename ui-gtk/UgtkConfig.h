eb440042fa2a683a5b200d3be58133e5e7c5e540
C.H. Huang <plushuang.tw@gmail.com>
2020-01-01 02:34:03 +0800

    bump version to 2.2.3

--- ui-gtk/UgtkConfig.h
+++ ui-gtk/UgtkConfig.h
@@ -52,7 +52,7 @@ extern "C" {
 #endif
 
 #ifndef PACKAGE_VERSION
-#define PACKAGE_VERSION		"2.2.2"
+#define PACKAGE_VERSION		"2.2.3"
 #endif
 
 // ----------------------------------------------------------------------------
